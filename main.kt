04891f76355d6db5d5fadce21401153cb265cbc3
Ekaterina <96653641+Podavalenko@users.noreply.github.com>
2023-01-18 02:49:34 +0000

    Initial commit

---
+++ main.kt
@@ -0,0 +1,14 @@
+fun main() = runBlocking {
+    val job = CoroutineScope(EmptyCoroutineContext).launch {
+        launch {
+            delay(500)
+            println("ok") // <--
+        }
+        launch {
+            delay(500)
+            println("ok")
+        }
+    }
+    delay(100)
+    job.cancelAndJoin()
+}
